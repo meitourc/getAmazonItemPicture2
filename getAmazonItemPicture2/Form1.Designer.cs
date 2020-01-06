d407c6bed959631e8fd5e2784a7e3aa836d76539
Takuma Yamada <meitourc@gmail.com>
2020-01-06 22:35:20 +0900

    20200106

--- getAmazonItemPicture2/Form1.Designer.cs
+++ getAmazonItemPicture2/Form1.Designer.cs
@@ -31,25 +31,29 @@
             this.button_csv_read = new System.Windows.Forms.Button();
             this.button_exec = new System.Windows.Forms.Button();
             this.textBox_input = new System.Windows.Forms.TextBox();
+            this.button_input_set = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
             // button_csv_read
             // 
-            this.button_csv_read.Location = new System.Drawing.Point(860, 94);
+            this.button_csv_read.Location = new System.Drawing.Point(978, 74);
             this.button_csv_read.Name = "button_csv_read";
             this.button_csv_read.Size = new System.Drawing.Size(292, 71);
             this.button_csv_read.TabIndex = 0;
             this.button_csv_read.Text = "CSV読み取り";
             this.button_csv_read.UseVisualStyleBackColor = true;
+            this.button_csv_read.Click += new System.EventHandler(this.button_csv_read_Click);
             // 
             // button_exec
             // 
-            this.button_exec.Location = new System.Drawing.Point(877, 235);
+            this.button_exec.Location = new System.Drawing.Point(919, 294);
             this.button_exec.Name = "button_exec";
             this.button_exec.Size = new System.Drawing.Size(403, 94);
             this.button_exec.TabIndex = 1;
             this.button_exec.Text = "実行";
             this.button_exec.UseVisualStyleBackColor = true;
+            this.button_exec.Click += new System.EventHandler(this.button_exec_Click);
             // 
             // textBox_input
             // 
@@ -58,11 +62,26 @@
             this.textBox_input.Size = new System.Drawing.Size(671, 31);
             this.textBox_input.TabIndex = 2;
             // 
+            // button_input_set
+            // 
+            this.button_input_set.Location = new System.Drawing.Point(759, 91);
+            this.button_input_set.Name = "button_input_set";
+            this.button_input_set.Size = new System.Drawing.Size(136, 36);
+            this.button_input_set.TabIndex = 3;
+            this.button_input_set.Text = "参照";
+            this.button_input_set.UseVisualStyleBackColor = true;
+            this.button_input_set.Click += new System.EventHandler(this.button_input_set_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1582, 668);
+            this.Controls.Add(this.button_input_set);
             this.Controls.Add(this.textBox_input);
             this.Controls.Add(this.button_exec);
             this.Controls.Add(this.button_csv_read);
@@ -79,6 +98,8 @@
         private System.Windows.Forms.Button button_csv_read;
         private System.Windows.Forms.Button button_exec;
         private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.Button button_input_set;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- getAmazonItemPicture2/Form1.cs
+++ getAmazonItemPicture2/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using getAmazonItemPicture2.Exceptions;
+using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,10 +10,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace getAmazonItemPicture2
 {
     public partial class Form1 : Form
     {
+
+        static string DELIMITER = ","; //CSV読み書き用区切り文字
+        static string DOUBLE_QUOTATION = "\""; //ダブルクォーテーション
+        static string LINE_FEED_CODE = "\r\n"; //改行コード
+        List<string> stringList = new List<string>(); //CSV読み込みデータ格納リスト
+
+
+
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +31,91 @@ namespace getAmazonItemPicture2
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+        }
+
+        /// <summary>
+        /// CSV読み取り
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_csv_read_Click(object sender, EventArgs e)
+        {
+            string path = @""; //入力ファイル名
+            char[] delimiter = DELIMITER.ToCharArray(); //区切り文字をまとめる
+            string[] strData; //分解後の文字の入れ物
+            string strLine; //1行分のデータ
+
+            path = textBox_input.Text;
+            path = @"C:\1_project\種市さん\amazon\takuma\takuma\input\input.csv";
+
+            bool fileExists = System.IO.File.Exists(path);
+            if (fileExists)
+            {
+
+                TextFieldParser parser = new TextFieldParser(path, System.Text.Encoding.Default);
+                var dataList = new List<string[]>();
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(DELIMITER);
+                //while (sr.Peek() >= 0)
+                while (parser.EndOfData == false)
+                {
+                    try
+                    {
+
+                        strData = parser.ReadFields();
+
+                        if (parser.LineNumber == 2 && strData[0] == "id")
+                        {
+                            continue;
+                        }
+
+ 
+
+                        stringList.AddRange(strData);
+
+
+
+                        Console.WriteLine(strData[1]);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("データの読み込みに失敗しました。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                parser.Close();
+            }
+
+
+        }
+
+        /// <summary>
+        /// 入力パス設定
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_input_set_Click(object sender, EventArgs e)
+        {
+            var result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.Cancel) throw new CancelException();
+            if (result == DialogResult.No) return;
+            textBox_input.Text = openFileDialog1.FileName;
+        }
+
+        /// <summary>
+        /// スクレイピング実行
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_exec_Click(object sender, EventArgs e)
+        {
+            // コンソールに出力する
+            foreach (string list in stringList)
+            {
+                //Console.WriteLine("{0} ", list);
+                Console.WriteLine(list[0]);
+            }
 
         }
     }
